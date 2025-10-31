234ca0ae91d0f718a7f2603d5f04ab9f80d40961
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2025-10-31 18:19:53 +0000

    Switched from types, to tags

--- util/slickHelper.lua
+++ util/slickHelper.lua
@@ -2,9 +2,14 @@ local slick = require("libs.slick")
 
 local slickHelper = { }
 
-slickHelper.types = {
+-- todo add sound variable, and play if character walks into such type
+-- Tags could define interaction too
+-- Thus to correctly compare tag.type == tag.type
+
+slickHelper.tags = {
   CHARACTER = slick.newEnum({ type = "character" }),
-  WALL = slick.newEnum({ type = "wall" }),
+  WALL      = slick.newEnum({ type = "wall" }),
+  LOG       = slick.newEnum({ type = "log" }),
 }
 
 return slickHelper
